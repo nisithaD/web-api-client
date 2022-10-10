03cc9358c98f9c1655e19101126feeedf2b2df15
Nisitha Dayarathna <nisitha56190@gmail.com>
2022-10-10 23:41:47 +0530

    Cart function minor changes added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import Layout from './Layout';
 import NoMatch from './pages/NoMatch';
-import { isAdmin, isLoggedIn } from './utils/session';
+import {isAdmin, isLoggedIn} from './utils/session';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import AdminRestaurant from './pages/admin/AdminRestaurant';
 import AdminRestaurantEdit from './pages/admin/AdminRestaurantEdit';
@@ -19,28 +19,30 @@ import Cart from './pages/Cart';
 import RestaurantsView from './pages/RestaurantsView';
 
 function App() {
-  console.log(isAdmin());
-  return (
-    <>
-      <Layout>
-        <Router>
-          <Routes>
-            <Route exact path='/' element={isLoggedIn() ? (<HomePage />) : (<Navigate to='/login' />)} />
-            <Route exact path='/login' element={<LoginPage />} />
-            <Route exact path='/admin/dashboard' element={isAdmin() ? (<AdminDashboard />) : (<Navigate to='/login' />)} />
-            <Route exact path='/admin/restaurants' element={isAdmin() ? (<AdminRestaurant />) : (<Navigate to='/login' />)} />
-            <Route exact path='/admin/restaurants/edit' element={isAdmin() ? (<AdminRestaurantEdit />) : (<Navigate to='/login' />)} />
-            <Route exact path='/admin/users' element={isAdmin() ? (<AdminUsers />) : (<Navigate to='/login' />)} />
-            <Route exact path='/admin/users/edit' element={isAdmin() ? (<AdminEditUsers />) : (<Navigate to='/login' />)} />
-           <Route exact path='/outlets' element={isLoggedIn() ? (<Outlets />) : (<Navigate to='/login' />)} />
-           <Route exact path='/outlets/view' element={isLoggedIn() ? (<RestaurantsView />) : (<Navigate to='/login' />)} />
-           <Route exact path='/cart' element={isLoggedIn() ? (<Cart />) : (<Navigate to='/login' />)} />
-            <Route path='*' element={<NoMatch />} />
-          </Routes>
-        </Router>
-      </Layout>
-    </>
-  );
+    return (
+        <Layout>
+            <Router>
+                <Routes>
+                    <Route exact path='/' element={isLoggedIn() ? (<HomePage/>) : (<Navigate to='/login'/>)}/>
+                    <Route exact path='/login' element={<LoginPage/>}/>
+                    <Route exact path='/admin/dashboard'
+                           element={isAdmin() ? (<AdminDashboard/>) : (<Navigate to='/login'/>)}/>
+                    <Route exact path='/admin/restaurants'
+                           element={isAdmin() ? (<AdminRestaurant/>) : (<Navigate to='/login'/>)}/>
+                    <Route exact path='/admin/restaurants/edit'
+                           element={isAdmin() ? (<AdminRestaurantEdit/>) : (<Navigate to='/login'/>)}/>
+                    <Route exact path='/admin/users' element={isAdmin() ? (<AdminUsers/>) : (<Navigate to='/login'/>)}/>
+                    <Route exact path='/admin/users/edit'
+                           element={isAdmin() ? (<AdminEditUsers/>) : (<Navigate to='/login'/>)}/>
+                    <Route exact path='/outlets' element={isLoggedIn() ? (<Outlets/>) : (<Navigate to='/login'/>)}/>
+                    <Route exact path='/outlets/view'
+                           element={isLoggedIn() ? (<RestaurantsView/>) : (<Navigate to='/login'/>)}/>
+                    <Route exact path='/cart' element={isLoggedIn() ? (<Cart/>) : (<Navigate to='/login'/>)}/>
+                    <Route path='*' element={<NoMatch/>}/>
+                </Routes>
+            </Router>
+        </Layout>
+    );
 }
 
 export default App;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {
     MDBBtn,
     MDBCard,
@@ -11,11 +11,36 @@ import {
     MDBRow,
     MDBTypography,
 } from "mdb-react-ui-kit";
+import axios from "axios";
+import API from "../config/api";
+import {loadState} from "../utils/session";
+import {decodeToken} from "react-jwt";
 
 export default function Cart() {
-    //get cart items
+    const [foods, setFoodItems] = useState();
+    useEffect(() => {
+        (async function () {
+            let token = loadState()['token'];
+            let user_id = decodeToken(token)._id;
+            try {
+                let foods = await axios.get(API.DOMAIN + '/api/users/' + user_id + '/cart', {
+                    headers: {
+                        "x-Authorization": loadState()['token']
+                    }
+                });
+                if (foods.status === 200) {
+                    setFoodItems(foods.data.data)
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        })();
+    }, []);
+
+    //todo have to complete
+
     return (
-        <section className="h-100" style={{ backgroundColor: "#eee" }}>
+        <section className="h-100" style={{backgroundColor: "#eee"}}>
             <MDBContainer className="py-5 h-100">
                 <MDBRow className="justify-content-center align-items-center h-100">
                     <MDBCol md="10">
@@ -27,42 +52,51 @@ export default function Cart() {
 
                         <MDBCard className="rounded-3 mb-4">
                             <MDBCardBody className="p-4">
-                                <MDBRow className="justify-content-between align-items-center">
-                                    <MDBCol md="2" lg="2" xl="2">
-                                        <MDBCardImage className="rounded-3" fluid
-                                                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                                                      alt="Cotton T-shirt" />
-                                    </MDBCol>
-                                    <MDBCol md="3" lg="3" xl="3">
-                                        <p className="lead fw-normal mb-2">Basic T-shirt</p>
-                                        <p>
-                                            <span className="text-muted">Price: </span>${125.00}
-                                            <span className="text-muted ms-3">Flavor: </span> Vanilla
-                                        </p>
-                                    </MDBCol>
-                                    <MDBCol md="3" lg="3" xl="2"
-                                            className="d-flex align-items-center justify-content-around">
-                                        <MDBBtn color="link" className="px-2">
-                                            <MDBIcon fas icon="minus" />
-                                        </MDBBtn>
+                                {foods && foods.map((res, key,) => {
 
-                                        <MDBInput min={0} defaultValue={2} type="number" size="sm" />
+                                        let food_id = res._id;
+                                        let restaurant_id = foods[key].outlet;
+                                        let price = foods[key].price;
+                                        let qty = foods[key].qty;
 
-                                        <MDBBtn color="link" className="px-2">
-                                            <MDBIcon fas icon="plus" />
-                                        </MDBBtn>
-                                    </MDBCol>
-                                    <MDBCol md="3" lg="2" xl="2" className="offset-lg-1">
-                                        <MDBTypography tag="h5" className="mb-0">
-                                            $499.00
-                                        </MDBTypography>
-                                    </MDBCol>
-                                    <MDBCol md="1" lg="1" xl="1" className="text-end">
-                                        <a href="#!" className="text-danger">
-                                            Remove
-                                        </a>
-                                    </MDBCol>
-                                </MDBRow>
+                                        return <MDBRow key={key} className="justify-content-between align-items-center mb-3">
+                                            <MDBCol md="2" lg="2" xl="2">
+                                                <MDBCardImage className="rounded-3" fluid
+                                                              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
+                                                              alt="Cotton T-shirt"/>
+                                            </MDBCol>
+                                            <MDBCol md="3" lg="3" xl="3">
+                                                <p className="lead fw-normal mb-2">{food_id}</p>
+                                                <p>
+                                                    <span className="text-muted">Price: </span>${price}
+                                                    <span className="text-muted ms-3">Flavor: </span> Vanilla
+                                                </p>
+                                            </MDBCol>
+                                            <MDBCol md="3" lg="3" xl="2"
+                                                    className="d-flex align-items-center justify-content-around">
+                                                <MDBBtn color="link" className="px-2">
+                                                    <MDBIcon fas icon="minus"/>
+                                                </MDBBtn>
+
+                                                <MDBInput min={0} defaultValue={qty} type="number" size="sm"/>
+
+                                                <MDBBtn color="link" className="px-2">
+                                                    <MDBIcon fas icon="plus"/>
+                                                </MDBBtn>
+                                            </MDBCol>
+                                            <MDBCol md="3" lg="2" xl="2" className="offset-lg-1">
+                                                <MDBTypography tag="h5" className="mb-0">
+                                                    ${price*qty}
+                                                </MDBTypography>
+                                            </MDBCol>
+                                            <MDBCol md="1" lg="1" xl="1" className="text-end">
+                                                <a href="#!" className="text-danger">
+                                                    Remove
+                                                </a>
+                                            </MDBCol>
+                                        </MDBRow>
+                                    }
+                                )}
                             </MDBCardBody>
                         </MDBCard>
 
